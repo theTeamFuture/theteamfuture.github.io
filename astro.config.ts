f758f4b29c7ed3f16dc30121e8fe2dca9aad30d4
SNRainiar <rainiar@foxmail.com>
2024-05-13 16:48:48 +0800

    chore(repo): initialize repo

---
+++ astro.config.ts
@@ -0,0 +1,20 @@
+/// Astro config
+import { defineConfig } from 'astro/config';
+
+// Integerations
+import icon from 'astro-icon';
+import tailwind from '@astrojs/tailwind';
+
+// Plugins
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+
+// Export config
+export default defineConfig({
+  integrations: [icon(), tailwind()],
+  markdown: {
+    rehypePlugins: [[rehypeKatex, { output: 'html' }]],
+    remarkPlugins: [remarkMath],
+    syntaxHighlight: 'prism'
+  }
+});
