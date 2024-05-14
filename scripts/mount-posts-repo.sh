222000933f7e78b29295503586e6913c6eaf8b3f
SNRainiar <rainiar@foxmail.com>
2024-05-15 01:14:55 +0800

    fix(ci): script error

--- scripts/mount-posts-repo.sh
+++ scripts/mount-posts-repo.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Mount authors
-rm -r src/contents/authors
+rm -r src/content/authors
 mv posts-repo/authors src/content/authors
+ls -la src/content/authors
 
 # Mount avatars
 rm -r src/assets/avatars
 mv posts-repo/avatars src/assets/avatars
+ls -la src/assets/avatars
 
 # Mount images
 rm -r src/assets/images
 mv posts-repo/images src/assets/images
+ls -la src/assets/images
 
 # Mount posts
-rm -r src/contents/posts/*
+rm -r src/content/posts/*
 for FOLDER_DIR in posts-repo/posts/*; do
   if [[ -d "$FOLDER_DIR" ]]; then
     # Get folder name
@@ -23,12 +26,13 @@ for FOLDER_DIR in posts-repo/posts/*; do
     HASH_NAME=$(echo -n $FOLDER_NAME | sha256sum | awk '{print $1}')
 
     # Move markdown to contents collection
-    mv $FOLDER_DIR"/post.md" "src/contents/posts/"$HASH_NAME".md"
+    mv $FOLDER_DIR"/post.md" "src/content/posts/"$HASH_NAME".md"
 
     # Move rest folder to contents collection with ignore
-    mv $FOLDER_DIR "src/contents/posts/_"$HASH_NAME
+    mv $FOLDER_DIR "src/content/posts/_"$HASH_NAME
 
     # Replace relative path
     sed -i "s/\.\//\.\/_"$HASH_NAME"\//g" "src/contents/posts/"$HASH_NAME".md"
   fi
 done
+ls -la src/content/posts
