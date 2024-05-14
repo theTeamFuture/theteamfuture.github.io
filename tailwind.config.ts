3c786e2bae4ae76737a78d9fe8a949d620a943f2
SNRainiar <rainiar@foxmail.com>
2024-05-14 21:05:37 +0800

    feat(component): add preloader and load bar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,16 @@
 /// Tailwind config
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 // Export config
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        misans: ['mi-sans', ...fontFamily.sans]
+      }
+    }
   },
   plugins: []
 } satisfies Config;
