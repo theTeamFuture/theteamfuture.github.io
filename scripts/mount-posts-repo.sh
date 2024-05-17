982c60e31a03664a6f19c688c62688044cbd9bbe
SNRainiar <rainiar@foxmail.com>
2024-05-17 17:53:07 +0800

    feat(post): add author info
    feat(time capsule): add time capsule
    
    Update about.md
    
    chore(ci): mount time capsule
    
    fix(ci): sed not found file, fail to mount time capsules
    
    chore(time-capsule): optimize component
    
    Fix mismatch link
    
    fix(avatar): local avatar path not resolved

--- scripts/mount-posts-repo.sh
+++ scripts/mount-posts-repo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Mount authors
-rm -r src/content/authors
+rm -rf src/content/authors
 mv posts-repo/authors src/content/authors
 rm src/content/authors/*.md
 
 # Mount avatars
-rm -r src/assets/avatars
+rm -rf src/assets/avatars
 mv posts-repo/avatars src/assets/avatars
 rm src/assets/avatars/*.md
 
 # Mount images
-rm -r src/assets/images
+rm -rf src/assets/images
 mv posts-repo/images src/assets/images
 rm src/assets/images/*.md
 
 # Mount posts
-rm -r src/content/posts/*
+rm -rf src/content/posts/*
 for FOLDER_DIR in posts-repo/posts/*; do
   if [[ -d "$FOLDER_DIR" ]]; then
     # Get folder name
@@ -32,6 +32,10 @@ for FOLDER_DIR in posts-repo/posts/*; do
     mv $FOLDER_DIR "src/content/posts/_"$HASH_NAME
 
     # Replace relative path
-    sed -i "s/\.\//\.\/_"$HASH_NAME"\//g" "src/contents/posts/"$HASH_NAME".md"
+    sed -i "s/\.\//\.\/_"$HASH_NAME"\//g" "src/content/posts/"$HASH_NAME".md"
   fi
 done
+
+# Mount time capsule
+rm -rf public/pool
+mv time-capsules-repo/pool public/pool
--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -1,4 +1,5 @@
 /// Meta module
+import type { ImageMetadata } from 'astro';
 import { getCollection } from 'astro:content';
 
 // Get posts
@@ -6,4 +7,9 @@ export const posts = await getCollection('posts');
 posts.sort(
   (a, b): number =>
     b.data.published_at.getTime() - a.data.published_at.getTime()
+);
+
+// Get avatars
+export const avatars = import.meta.glob<{ default: ImageMetadata }>(
+  '/src/assets/avatars/*'
 );
