d3623a01e63d087fa318d0efa18fd061ec7fe6ef
SNRainiar <rainiar@foxmail.com>
2024-05-17 17:20:59 +0800

    test(ci): add some debug in CI

--- scripts/mount-posts-repo.sh
+++ scripts/mount-posts-repo.sh
@@ -4,29 +4,30 @@
 rm -r src/content/authors
 mv posts-repo/authors src/content/authors
 rm src/content/authors/*.md
-ls -la src/content/authors
 
 # Mount avatars
 rm -r src/assets/avatars
 mv posts-repo/avatars src/assets/avatars
 rm src/assets/avatars/*.md
-ls -la src/assets/avatars
 
 # Mount images
 rm -r src/assets/images
 mv posts-repo/images src/assets/images
 rm src/assets/images/*.md
-ls -la src/assets/images
 
 # Mount posts
 rm -r src/content/posts/*
 for FOLDER_DIR in posts-repo/posts/*; do
   if [[ -d "$FOLDER_DIR" ]]; then
+    echo $FOLDER_DIR
+
     # Get folder name
     FOLDER_NAME=${FOLDER_DIR##*/}
+    echo $FOLDER_NAME
 
     # Get folder name SHA256
     HASH_NAME=$(echo -n $FOLDER_NAME | sha256sum | awk '{print $1}')
+    echo $HASH_NAME
 
     # Move markdown to contents collection
     mv $FOLDER_DIR"/post.md" "src/content/posts/"$HASH_NAME".md"
