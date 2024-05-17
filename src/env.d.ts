51ba09e7b6d1edc4aec4a99b5d0612fdc84dee17
SNRainiar <rainiar@foxmail.com>
2024-05-17 15:07:36 +0800

    feat(layout): add page layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// Rehype figure type define
+declare module 'rehype-figure' {
+  export default (option: any) => (tree: Root, file: VFile) => undefined;
+}
