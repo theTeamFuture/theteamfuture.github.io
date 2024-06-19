e6e7210e825ea58956142e821660434cb313e267
SNRainiar <rainiar@foxmail.com>
2024-06-19 17:36:50 +0800

    fix: define types for meta

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -1,19 +1,22 @@
 /// Meta module
 import type { ImageMetadata } from 'astro';
-import fs from 'fs';
+import type { CollectionEntry } from 'astro:content';
 import { getCollection } from 'astro:content';
+import fs from 'fs';
 
 // Get posts
-export const posts = await getCollection('posts');
+export const posts: CollectionEntry<'posts'>[] = await getCollection('posts');
 posts.sort(
-  (a, b): number =>
+  (a: CollectionEntry<'posts'>, b: CollectionEntry<'posts'>): number =>
     b.data.published_at.getTime() - a.data.published_at.getTime()
 );
 
 // Get puzzles
-export const puzzles = await getCollection('puzzles');
+export const puzzles: CollectionEntry<'puzzles'>[] = await getCollection(
+  'puzzles'
+);
 puzzles.sort(
-  (a, b): number =>
+  (a: CollectionEntry<'puzzles'>, b: CollectionEntry<'puzzles'>): number =>
     b.data.published_at.getTime() - a.data.published_at.getTime()
 );
 
@@ -25,14 +28,20 @@ export const avatars = import.meta.glob<{ default: ImageMetadata }>(
 // Generate meta data for build
 if (import.meta.env.PROD) {
   const postIdSlugMap: Record<string, string> = posts.reduce(
-    (prev: Record<string, string>, { id, slug }): Record<string, string> => {
+    (
+      prev: Record<string, string>,
+      { id, slug }: { id: string; slug: string }
+    ): Record<string, string> => {
       prev[id] = slug;
       return prev;
     },
     {}
   );
   const puzzleIdSlugMap: Record<string, string> = puzzles.reduce(
-    (prev: Record<string, string>, { id, slug }): Record<string, string> => {
+    (
+      prev: Record<string, string>,
+      { id, slug }: { id: string; slug: string }
+    ): Record<string, string> => {
       prev[id] = slug;
       return prev;
     },
