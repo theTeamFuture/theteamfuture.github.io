755f6990f06045296ad5370d3c6c43b1b08b5839
SNRainiar <rainiar@foxmail.com>
2025-03-01 23:53:15 +0800

    fix: figure plugin

--- rehype-plugins/figure.ts
+++ rehype-plugins/figure.ts
@@ -5,32 +5,53 @@ import { visit } from "unist-util-visit";
 // Export plugin
 export default function rehypeFigure(): Transformer<Root> {
   return (tree: Root): void => {
-    visit(tree, "img", (node: Element, index?: number): void => {
-      const figure: Element = {
-        type: "element",
-        tagName: "figure",
-        properties: {},
-        children: [node],
-      };
+    visit(tree, { tagName: "p" }, (node: Element, index?: number): void => {
+      const figures: Element[] = node.children
+        .filter((el: any): boolean => el.tagName === "img")
+        .map((el: any): Element => {
+          const figure: Element = {
+            type: "element",
+            tagName: "figure",
+            properties: {},
+            children: [el],
+          };
 
-      if (
-        typeof node.properties.alt === "string" &&
-        node.properties.alt.trim().length > 0
-      ) {
-        figure.children.push({
-          type: "element",
-          tagName: "figcaption",
-          properties: {},
-          children: [
-            {
-              type: "text",
-              value: node.properties.alt.trim(),
-            },
-          ],
+          if (
+            typeof el.properties.alt === "string" &&
+            el.properties.alt.trim().length > 0
+          ) {
+            figure.children.push({
+              type: "element",
+              tagName: "figcaption",
+              properties: {},
+              children: [
+                {
+                  type: "text",
+                  value: el.properties.alt.trim(),
+                },
+              ],
+            });
+          }
+
+          return figure;
         });
+
+      if (figures.length === 0) {
+        return;
       }
 
-      tree.children[index!] = figure;
+      tree.children[index!] =
+        figures.length === 1
+          ? figures[0]
+          : {
+              type: "element",
+              tagName: "div",
+              properties: {
+                class: "figures-container",
+              },
+              children: figures,
+            };
+      console.log(tree);
     });
   };
 }
